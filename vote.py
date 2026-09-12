e4f5850cc774e5f545b5f708549d0753f95b3fa5
ravimanasa71-web <ravimanasa71@gmail.com>
2026-09-12 11:58:27 +0530

    updated vote.py

--- vote.py
+++ vote.py
@@ -1,5 +1,7 @@
 age=int(input("Enter your age:"))
 if age>=18:
   print("you are eligible to vote:")
+  print("you can cast your vote")
 else:
   print("you are not eligible to vote:")
+  print("you cannot cast your vote")
